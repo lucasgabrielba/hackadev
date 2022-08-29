9a8b0ff3bc1fee404b3fbee47a544829f322c871
Lucas Gabriel <lucasgabrielba@gmail.com>
2022-08-29 08:47:12 -0300

    fisrt commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Navbar from "./components/Navbar/Navbar";
+import Main from "./components/Main/Main";
+import Boxes from "./components/Boxes/Boxes";
+import Products from "./components/Products/Products";
+import Shop from "./components/Shop/Shop";
+import Footer from "./components/Footer/Footer";
+import { useState } from "react";
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Main />
+      <Boxes />
+      <Products />
+      <Shop />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
